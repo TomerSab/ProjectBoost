8f1dedf3d35fd4176cefa8c04d9b20539432aea1
Tomer Sabag <Tomerr42@gmail.com>
2020-06-26 13:10:24 +0300

    Background Boost Rotationts

---
+++ Assets/Rocket.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField]float rotatBoost = 100f;
+    [SerializeField]float mainBoost = 100f;
+    Rigidbody rigidBody;
+    AudioSource boostAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        boostAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        Booosting();
+        Rotate();
+    }
+
+    private void Booosting()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            rigidBody.AddRelativeForce(Vector3.up * mainBoost);
+            if (!boostAudio.isPlaying)
+            {
+                boostAudio.Play();
+            }
+        }
+        else boostAudio.Stop();
+    }
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // Take  Manual Control
+
+        
+        float rotationsThisFrame = rotatBoost * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+            transform.Rotate(Vector3.forward* rotationsThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+
+            transform.Rotate(-Vector3.forward* rotationsThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; // Take  Manual Control
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK"); //TODO Remove
+                break;
+            case "Fuel":
+                print("Fuel");//TODO Remove
+                break;
+            default:
+                print("Dead");
+                break;
+        }
+    }
+}
